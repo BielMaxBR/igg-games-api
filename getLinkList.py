3f29d1f213f4a34b1ecf42de7fa02c61c6404d0f
Gabriel Maximo <gbielmaximo@gmail.com>
2020-04-23 01:14:16 +0000

    meeeeee

--- getLinkList.py
+++ getLinkList.py
@@ -3,24 +3,41 @@ from bs4 import BeautifulSoup
 import re
 from getSearchList import getSearchList
 
-
+# game = URL do game no igg-games.com
 def getLinkList(game):
   req = requests.get(f'{game}')
+  print(req.status_code)
   soup = BeautifulSoup(req.content, 'html.parser')
   List = []
-  for link in soup.find_all("b", class_="uk-heading-bullet"):
-    Plink = link.parent()
-    
-    title = str(Plink.find("b"))
-    # Alink = str(Plink.find("a")['href'])
-    # AAlink = str(Plink.find("a")['href'])[Alink.index('url='):]
-
+  for link_list in soup.find_all("b", class_="uk-heading-bullet"):
+    Plink = link_list.parent()
+    # Plink = Plink[0]
+    title = str(Plink[0].string)
+    link = Plink[2]
+    if str(link).count('blue') != 0:
+        print(str(link).count('blue'))
+        finalLink = str(link['href'])[60:]
+    else:
+        print('adfly', str(link).count('blue'))
+        print('#'*60)
+        finalLink = str(link['href'])
+    print('Link=', finalLink)
+    print('$'*60)
+    # finalLink = link['href']
     game = {
       "title": title,
-      # "link": AAlink,
+      "link": finalLink,
     }
 
     List.append(game)
   return List
+"""
+USkSn82FylsejFCipVsahU2r2FXfgX2LgYHme3?xurl=
 
-print(getLinkList(getSearchList('UNO')[5]['link']))
+
+jFCipVhU2r2FXX2LgYHme3?xurl=
+jFCipVhU2r2FXX2LgYHme3?xurl=
+jFCipVhU2r2FXX2LgYHme3?xurl=
+jFCipVhU2r2FXX2LgYHme3?xurl=
+jFCipVhU2r2FXX2LgYHme3?xurl=
+"""
---
+++ test.py
@@ -0,0 +1,4 @@
+from getLinkList import getLinkList
+from getSearchList import getSearchList
+
+print(getLinkList("http://www.igg-games.com/vvvvvv-403518069-free-download.html"))
